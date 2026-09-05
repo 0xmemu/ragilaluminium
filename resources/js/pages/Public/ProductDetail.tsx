cca7dff4bc070426ada98fff2c1b86da90f1dfbc
Ubuntu <ubuntu@localhost.localdomain>
2026-09-05 23:53:18 +0000

    fix(pdp): kunci lebar kolom dan galeri desktop presisi 480px (0.8x) tanpa terpengaruh lebar kontainer

--- resources/js/pages/Public/ProductDetail.tsx
+++ resources/js/pages/Public/ProductDetail.tsx
@@ -131,7 +131,7 @@ export default function ProductDetail({
 
 
       <section className="container-page !px-2.5 md:!px-8 lg:!px-12 pb-4 pt-0 lg:pt-10 lg:pb-10">
-        <div className="grid min-w-0 gap-2 lg:grid-cols-[minmax(0,490px)_minmax(0,1fr)] lg:items-start lg:gap-8 xl:gap-12">
+        <div className="grid min-w-0 gap-2 lg:grid-cols-[480px_minmax(0,1fr)] lg:items-start lg:gap-8">
           {/* Kolom Kiri: Galeri Produk + (Desktop) Ulasan Pembeli & Hasil Pemasangan */}
           <div className="relative min-w-0">
             <button
